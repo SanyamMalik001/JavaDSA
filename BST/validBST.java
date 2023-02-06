f02dd2fbcedf14694d7f74a9900803e2ed9ca9d4
Sanyam Malik <50053596+SanyamMalik001@users.noreply.github.com>
2023-02-06 17:43:36 +0530

    Update validBST.java

--- BST/validBST.java
+++ BST/validBST.java
@@ -8,3 +8,22 @@ static Integer prev=null;
         if(!validateBST(root.right)) return false;
         return true;
     }
+// The first method is using inorder to check if the next value is greater than previous one as it is inorder of a BST.
+// The problem with above solution is it cannot handle duplicate values consecutively, so we have to consider that there are none.
+
+
+
+
+
+
+public static boolean validateBST(BinaryTreeNode<Integer> root){
+         return validateBST(root, Integer.MAX_VALUE, Integer.MIN_VALUE);
+
+    }
+    public static boolean validateBST(BinaryTreeNode<Integer> root, int max, int min){
+        if(root==null) return true;
+        if(root.data<min||root.data>=max) return false;
+        if(!validateBST(root.left,min,root.data)||!validateBST(root.right,root.data,max)) return false;
+        return true;
+    }
+// In this solution time is O(n) and space is O(D).
